0591b47fbee88e1dd9e62a55d8be89d40f770e1f
Lakshmi Vidyasagar <lakshmi@thetrycatch.com>
2025-01-01 09:27:23 +0530

    remove unused imports from app routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { ThemeSelectorComponent } from '../theme-selector/theme-selector.component';
-import { AppFrameComponent } from '../app-frame/app-frame.component';
 import { FooComponent } from '../foo/foo.component';
 
 export const routes: Routes = [
